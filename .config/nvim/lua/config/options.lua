8aa31f7b7bd79509a2e6d7267c6d257c75f9257d
rdk-codes <rdineshkartikrdk@gmail.com>
2025-05-29 22:44:16 +0530

    changes to tmux

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -8,28 +8,28 @@ opt.relativenumber = true
 opt.number = true
 
 -- tabs & indentation
-opt.tabstop = 2 
-opt.shiftwidth = 2 
-opt.expandtab = true 
-opt.autoindent = true 
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
 
 opt.wrap = false
 
 -- search settings
 opt.ignorecase = true
-opt.smartcase = true 
+opt.smartcase = true
 
 opt.cursorline = false
 opt.termguicolors = true
-opt.signcolumn = "yes" 
+opt.signcolumn = "yes"
 
 -- backspace
-opt.backspace = "indent,eol,start" 
+opt.backspace = "indent,eol,start"
 
 -- split windows
 opt.splitright = true
-opt.splitbelow = true 
+opt.splitbelow = true
 opt.swapfile = false
 
--- fix annoying 
+-- fix annoying
 vim.opt.fillchars:append { eob = " " }
