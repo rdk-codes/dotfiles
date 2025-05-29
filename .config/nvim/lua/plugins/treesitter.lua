87be5f9ae57e9b26d0ac4bce17cdac1a011cf435
rdk-codes <rdineshkartikrdk@gmail.com>
2025-05-29 20:36:17 +0530

    added harpoon

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,22 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "python", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+      ensure_installed = {
+        "python",
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "markdown",
+        "markdown_inline",
+        "json",
+        "hyprlang",
+        "html",
+        "css",
+        "csv",
+        "bash"
+      },
       sync_install = false,
       auto_install = true,
       highlight = {
